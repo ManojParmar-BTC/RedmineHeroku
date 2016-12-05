7b7c93d7650daaa6bf669c9e1e48ab0bf3483f9e
ManojParmar-BTC <manoj.parmar@botreetechnologies.com>
2016-12-05 18:10:51 +0530

    display files changes for pr

--- plugins/btc_github_pr/config/routes.rb
+++ plugins/btc_github_pr/config/routes.rb
@@ -1,4 +1,5 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-get 'pull_requests', :to => 'pull_requests#index'
+get 'pull_requests', to: 'pull_requests#index'
+get 'pull_requests/view_changed_files/:id', to: 'pull_requests#view_changed_files', as: "view_changed_files" 
 post 'pull_requests/github_hook', :to => 'pull_requests#github_hook'
